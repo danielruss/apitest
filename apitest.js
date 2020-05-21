37495230dc0312e54d4daabe79cabf72a7f27872
Daniel Russ <druss@nih.gov>
2020-05-21 17:20:10 -0400

    initial commit

---
+++ apitest.js
@@ -0,0 +1,9 @@
+console.log("in apitest.js");
+// let script = document.createElement("script");
+// script.src = "mytestconfig.js";
+
+window.onload = function (event) {
+  console.log("in apitest.js:window.onload");
+
+  //document.getElementById("q").innerHTML = config.fun1a();
+};
